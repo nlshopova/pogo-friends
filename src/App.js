a4e8794283ad2ff631c8f9711ac62dd15e3ddb9c
Nikolina Shopova <nikolina.shopova@gmail.com>
2018-07-27 17:23:38 +0200

    level increment complete

--- src/App.js
+++ src/App.js
@@ -10,11 +10,23 @@ class App extends Component {
 
     this.state = {
       friends: [],
-    };
+    }
+  }
+  async markActive(id) {
+    let currentDate = new Date();
+    let currentLevel=0;
+    await firestore.collection("friends").doc(id).get().then(function(doc) {
+      currentLevel = doc.data().level;
+    });
+    currentLevel+=1;
+   await firestore.collection("friends").doc(id).set({
+        lastDay: currentDate,
+        level: currentLevel
+      },{ merge: true });
   }
 
-  componentWillMount() {
-
+  componentDidMount() {
+ 
     firestore.collection("friends").onSnapshot(snapshot => {
       let friends = [];
       snapshot.forEach(doc => {
@@ -22,18 +34,26 @@ class App extends Component {
         friend.id = doc.id;
         friends.push(friend);
       });
+
+      friends.map(friend => {
+        let currentDate = new Date();
+        if(friend.lastDay.getDate() !== currentDate.getDate()){return {...friend, activeToday:false}}
+        else{return {...friend, activeToday:true}} 
+      })
+      
       this.setState({ friends:friends });
     });
-
   }
+  
 
   render() {
+    // console.log(this.state.friends);
     return (
       <div className="App">
          {
             this.state.friends.map((friend) => {
               return (
-               <p key={friend.id}>{friend.nickname}</p>
+               <p key={friend.id}>{friend.nickname} <button  onClick={evt => this.markActive(friend.id)}>Activity</button></p>
               )
             })
           }
