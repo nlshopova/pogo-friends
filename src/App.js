c957fc11a1d059bce791bc3e7c999fa3819c76e6
Nikolina Shopova <nikolina.shopova@gmail.com>
2018-07-28 10:59:26 +0200

    active today status

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import firestore from "./firestore";
-import './App.css';
-
-
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      friends: [],
-    }
+      friends: []
+    };
   }
   async markActive(id) {
     let currentDate = new Date();
-    let currentLevel=0;
-    await firestore.collection("friends").doc(id).get().then(function(doc) {
-      currentLevel = doc.data().level;
-    });
-    currentLevel+=1;
-   await firestore.collection("friends").doc(id).set({
-        lastDay: currentDate,
-        level: currentLevel
-      },{ merge: true });
+    let currentLevel = 0;
+    await firestore
+      .collection("friends")
+      .doc(id)
+      .get()
+      .then(function(doc) {
+        currentLevel = doc.data().level;
+      });
+    currentLevel += 1;
+    await firestore
+      .collection("friends")
+      .doc(id)
+      .set(
+        {
+          lastDay: currentDate,
+          level: currentLevel
+        },
+        { merge: true }
+      );
   }
 
   componentDidMount() {
- 
     firestore.collection("friends").onSnapshot(snapshot => {
       let friends = [];
       snapshot.forEach(doc => {
@@ -35,28 +42,33 @@ class App extends Component {
         friends.push(friend);
       });
 
-      friends.map(friend => {
+      let friends2 = friends.map(friend => {
         let currentDate = new Date();
-        if(friend.lastDay.getDate() !== currentDate.getDate()){return {...friend, activeToday:false}}
-        else{return {...friend, activeToday:true}} 
-      })
-      
-      this.setState({ friends:friends });
+        if (friend.lastDay.getDate() !== currentDate.getDate()) {
+          return { ...friend, activeToday: false };
+        } else {
+          return { ...friend, activeToday: true };
+        }
+      });
+
+      this.setState({ friends: friends2 });
     });
   }
-  
 
   render() {
-    // console.log(this.state.friends);
+    console.log(this.state.friends);
     return (
       <div className="App">
-         {
-            this.state.friends.map((friend) => {
-              return (
-               <p key={friend.id}>{friend.nickname} <button  onClick={evt => this.markActive(friend.id)}>Activity</button></p>
-              )
-            })
-          }
+        {this.state.friends.map(friend => {
+          return (
+            <p key={friend.id}>
+              {friend.nickname}{" "}
+              <button onClick={evt => this.markActive(friend.id)} disabled={friend.activeToday}>
+                Activity
+              </button>
+            </p>
+          );
+        })}
       </div>
     );
   }
